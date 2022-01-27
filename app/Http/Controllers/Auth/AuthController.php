139769b04d2344e0120afd5a56ba225af77a0e68
igadon <igadon@me.com>
2022-01-27 20:52:18 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\User;
 class AuthController extends Controller
 {
 
@@ -22,14 +22,44 @@ class AuthController extends Controller
     public function login(LoginFormRequest $request) {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->route('home')->with(
-                'login_success', 'ログイン成功しました。');
+        // アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
+
+        if(!is_null($user)) {
+            if ($user->locked_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                if ($user->error_count > 0 ) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with(
+                    'success',
+                    'ログイン成功しました。'
+                );
+            }
+
+            $user->error_count++;
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営者に連絡して下さい。',
+                ]);
+
+            }
+
+            $user->save();
         }
 
         return back()->withErrors([
-            'login_error' => 'メールアドレスかパスワードが間違っています。',
+            'danger' => 'メールアドレスかパスワードが間違っています。',
         ]);
     }
 
